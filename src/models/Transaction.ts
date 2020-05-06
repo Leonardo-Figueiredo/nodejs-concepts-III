5a0890e66ea517cdffa057611aa9a1dd01d9d499
Leonardo-Figueiredo <leo.nardorf22@gmail.com>
2020-05-06 01:40:17 -0300

    Category id bug

--- src/models/Transaction.ts
+++ src/models/Transaction.ts
@@ -1,16 +1,35 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  ManyToOne,
+  JoinColumn,
+} from 'typeorm';
+
+import Category from './Category';
+
+@Entity('transactions')
 class Transaction {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column()
   title: string;
 
+  @Column()
   type: 'income' | 'outcome';
 
+  @Column()
   value: number;
 
-  category_id: string;
+  @ManyToOne(() => Category)
+  @JoinColumn({ name: 'id' })
+  category_id: Category;
 
+  @Column()
   created_at: Date;
 
+  @Column()
   updated_at: Date;
 }
 
